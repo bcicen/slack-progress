bf3a9bb6fc0a8fb5c7a797d7ecdd597e113aa706
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-09-12 10:21:41 -0400

    initial code commit

---
+++ slack_progress/__init__.py
@@ -0,0 +1,23 @@
+from slacker import Slacker
+
+class SlackProgress(object):
+    def __init__(self, token, channel, start_pos=0, suffix='%'):
+        self.suffix = suffix
+        self.channel = channel
+        self.slack = Slacker(token)
+
+        res = self.slack.chat.post_message(
+                self.channel,
+                self.make_bar(start_pos)
+              )
+        self.msg_ts = res.body['ts']
+        self.channel_id = res.body['channel']
+
+    def update_bar(self, pos):
+        self.slack.chat.update(self.channel_id,
+                               self.msg_ts,
+                               self.make_bar(pos))
+
+    def make_bar(self, pos): 
+        bar = (round(pos / 5) * chr(9608))
+        return '{} {}'.format(bar, pos + self.suffix)
