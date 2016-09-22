26531afe286aed4766a66bcc5fb52f808d3ec7c2
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-09-22 02:53:26 -0400

    add new() method, update usage example

--- README.md
+++ README.md
@@ -12,14 +12,16 @@ pip install slack-progress
 
 ## Usage
 
+### Manual
+The % completed can also be set manually:
+
 ```python
-from time import sleep
 from slack_progress import SlackProgress
 
-progress_bar = SlackProgress('SLACK_TOKEN', 'CHANNEL_NAME')
+sp = SlackProgress('SLACK_TOKEN', 'CHANNEL_NAME')
+pbar = sp.new()
 for i in range(1,101):
-    s.update_bar(a)
-    sleep(.2)
+    pbar.update(i)
 ```
 
 [screencap]: http://i.imgur.com/103z4Io.gif "slack-progress"
--- slack_progress/__init__.py
+++ slack_progress/__init__.py
@@ -1,23 +1,30 @@
 from slacker import Slacker
 
 class SlackProgress(object):
-    def __init__(self, token, channel, start_pos=0, suffix='%'):
+    def __init__(self, token, channel, suffix='%'):
         self.suffix = suffix
         self.channel = channel
         self.slack = Slacker(token)
 
-        res = self.slack.chat.post_message(
-                self.channel,
-                self.make_bar(start_pos)
-              )
-        self.msg_ts = res.body['ts']
-        self.channel_id = res.body['channel']
+    def new(self):
+        res = self.slack.chat.post_message(self.channel, self._makebar(0))
+        bar = ProgressBar(self)
+        bar.msg_ts = res.body['ts']
+        bar.channel_id = res.body['channel']
+        return bar
 
-    def update_bar(self, pos):
-        self.slack.chat.update(self.channel_id,
-                               self.msg_ts,
-                               self.make_bar(pos))
+    def _update(self, chan, msg_ts, pos):
+        self.slack.chat.update(chan, msg_ts, self._makebar(pos))
 
-    def make_bar(self, pos): 
+    def _makebar(self, pos): 
         bar = (round(pos / 5) * chr(9608))
         return '{} {}{}'.format(bar, pos, self.suffix)
+
+class ProgressBar(object):
+    msg_ts = None
+    channel_id = None
+    def __init__(self, sp):
+        self._sp = sp
+
+    def update(self, pos):
+        self._sp._update(self.channel_id, self.msg_ts, pos)
