4f09be61245a88b788d9d0b67fd30dfda40b442c
Bradley Cicenas <bradley@vektor.nyc>
2017-11-03 00:25:35 +0000

    v0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='slack-progress',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     description='A realtime progress bar for Slack',
     author='Bradley Cicenas',
