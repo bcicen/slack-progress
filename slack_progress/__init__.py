24ec2461d9ddec2a14144ead0acb0f365dfff11e
Garrett Edwards <garrettedwards95@gmail.com>
2018-08-29 11:26:12 -0400

    Add support for logging messages below the progress bar

--- slack_progress/__init__.py
+++ slack_progress/__init__.py
@@ -1,4 +1,7 @@
+import time
+
 from slacker import Slacker
+
 
 class SlackProgress(object):
     def __init__(self, token, channel, suffix='%'):
@@ -29,12 +32,16 @@ class SlackProgress(object):
             yield(item)
             bar.done = idx
 
-    def _update(self, chan, msg_ts, pos):
-        self.slack.chat.update(chan, msg_ts, self._makebar(pos))
+    def _update(self, chan, msg_ts, pos, msg_log):
+        text = list(msg_log)
+        text.insert(0, self._makebar(pos))
+        text = '\n'.join(text)
+        self.slack.chat.update(chan, msg_ts, text)
 
     def _makebar(self, pos):
         bar = (round(pos / 5) * chr(9608))
         return '{} {}{}'.format(bar, pos, self.suffix)
+
 
 class ProgressBar(object):
 
@@ -46,6 +53,7 @@ class ProgressBar(object):
         self._pos = 0
         self._done = 0
         self.total = total
+        self._msg_log = []
 
     @property
     def done(self):
@@ -63,5 +71,10 @@ class ProgressBar(object):
     @pos.setter
     def pos(self, val):
         if val != self._pos:
-            self._sp._update(self.channel_id, self.msg_ts, val)
             self._pos = val
+            self._sp._update(self.channel_id, self.msg_ts, self._pos, self._msg_log)
+
+    def log(self, msg):
+        timestamp = time.strftime('%X - ')  # returns HH:MM:SS time
+        self._msg_log.append(timestamp + msg)
+        self._sp._update(self.channel_id, self.msg_ts, self._pos, self._msg_log)
